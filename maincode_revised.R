9cad701e0fff1cef91a70001891ae2f8cd1e79b8
Jiwoo Kim <151223322+JiwooKim-CI@users.noreply.github.com>
2024-06-05 14:48:15 +0900

    Update maincode_revised.R

--- maincode_revised.R
+++ maincode_revised.R
@@ -1,4 +1,4 @@
-###import packages
+#import packages
 
 library(rvest)
 library(xml2)
@@ -6,20 +6,23 @@ library(R3port)
 library(pagedown)
 
 # set URL for crawling
-webpage <- read_html("HTML_2024.html")
+## In this case, we wrote "HTML_2024.html", since we loaded papers pubilshed in 2024.
+webpage <- read_html("HTML_2024.html") 
 
 # deleting references
+## We first find references using nodes for reference
 reference_nodes <- xml_find_all(webpage, "//a[starts-with(@id, 'c')]/following-sibling::p[@class='body-paragraph']")
-
+## Delete references from html file.
 for (node in reference_nodes) {
   xml_remove(node)
 }
 
+# save papers without references
 anal<-as.character(webpage)|>
   read_html()
 ###################################################################
-## counting 'mediation analysis'
-# set target word
+# counting 'mediation analysis'
+## set target word
 target_words <- c(" mediate", "Baron & Kenny", "indirect effect", "Sobel", "mediation", "Baron and Kenny")
 # counting total paper numbers
 papers <- html_nodes(anal, "div.print-citation")
@@ -27,7 +30,7 @@ paper_without_content <- html_nodes(anal, "dl.print-citation")
 
 target_word_count <- 0
 
-# iteration
+## iteration for counting papers with target word
 for (i in 1:length(papers)) {
   paper_text <- html_text(papers[i])
   paper_subtitles <- html_text(html_nodes(papers[i], "a[title^='Study'], a[title^='Studies'], a[title^='Experiment'], a[title^='Experiments']"))
@@ -43,15 +46,17 @@ for (i in 1:length(papers)) {
 
 cat("Papers that include words '", target_words, "'are:", target_word_count, "\n")
 
-## save papers
+# save papers
 paper_fnl <- list()
 paper_fnl_text <- list()
 
 for (i in 1:length(papers)) {
   paper <- papers[i]
   paper_text <- html_text(papers[i])
+  ## save words that specifying study ID
   paper_subtitles <- html_text(html_nodes(papers[i],"a[title^='Study'], a[title^='Studies'], a[title^='Experiment'], a[title^='Experiments']"))
   paper_discussion <- html_text(html_nodes(papers[i],"a[title^='Discussion'], a[title^='General Discussion']"))
+  ## save study parts only
   text <- strsplit(paper_text,paste0("\n ",paper_subtitles[1],"\n"),fixed = TRUE)[[1]][2] 
   text <- strsplit(text, paste0("\n ",paper_discussion[length(paper_discussion)],"\n"),fixed = TRUE)[[1]][1]
   if (any(grepl(paste(target_words, collapse = "|"), text, ignore.case = TRUE))) {
@@ -62,69 +67,21 @@ for (i in 1:length(papers)) {
     paper_pdfname <- paste0("paper_", title, ".pdf")
     paper_fnl <- append(paper_fnl,paper)
     paper_fnl_text<-append(paper_fnl_text,paper_text)
-    # save each paper as a html file
-    #paper_html <- as.character(paper[[1]])
-    #writeLines(paper_html, con = paper_filename)
   }
 }
 
-####convert html file to pdf
-
-html_folder <- getwd()
-pdf_folder <-  getwd()
-xml_folder <-  getwd()
-
-html_files <- list.files(html_folder, pattern = "^paper_.*\\.html$", full.names = TRUE)
-
-
-for (html_file in html_files) {
-  
-  pdf_file <- file.path(pdf_folder, sub(".html$", ".pdf", basename(html_file)))
-  
-  try({
-    chrome_print(
-      input = html_file,
-      output = pdf_file
-    )
-  }, silent = TRUE)
-}
-
-xml_output <- "output.xml"
-
-# XML 파일을 만들기 위한 빈 XML 객체 생성
-xml_doc <- xml2::xml_new_document()
-
-# HTML 파일을 읽어서 XML에 추가
-for (html_file in html_files) {
-  # HTML 파일 읽기
-  html_content <- read_html(html_file)
-  
-  # 필요한 정보 추출 (예시: 모든 링크 추출)
-  links <- html_content %>%
-    html_nodes("a") %>%
-    html_attr("href")
-  
-  # XML에 정보 추가
-  for (link in links) {
-    xml2::xml_add_child(xml_doc, "link", link)
-  }
-}
-
-# XML 파일 저장
-xml2::write_xml(xml_doc, xml_output)
 ###################################################################
-####automation
-#### dividing each paper by its studies
+# automation
+## dividing each paper by its studies
 SLR <- NULL
 SLR_fulltext <- NULL
-
 subjects <- NULL
 DOIs <- NULL
 Field <- NULL
 studies <- NULL
 paper_text <- NULL
+## save study parts only
 for (i in 1:length(paper_fnl)){
-
   paper_subtitles <- html_text(html_nodes(paper_fnl[[i]],"a[title^='Study'], a[title^='Studies'], a[title^='Experiment'], a[title^='Experiments']"))
   paper_discussion <- html_text(html_nodes(paper_fnl[[i]],"a[title^='Discussion'], a[title^='General Discussion']"))
   paper_texts <- html_text(paper_fnl[[i]])
@@ -137,6 +94,7 @@ for (i in 1:length(paper_fnl)){
       else {
         text <- strsplit(text, paste0("\n ",paper_discussion[length(paper_discussion)],"\n"),fixed = TRUE)[[1]][1]
 }
+      ## save paper's information
       subjects <- c(subjects, strsplit(paper_texts, "\n",fixed = TRUE)[[1]][2])
       DOIs <- c(DOIs,strsplit(paper_texts, "Digital Object Identifier:",fixed = TRUE)[[1]][2])
       studies <- c(studies, paper_subtitles[j])
@@ -172,9 +130,10 @@ for (i in 1:length(paper_fnl_text)){
   Field <- c(Field,strsplit(paper_texts, "PsycInfo Classification:",fixed = TRUE)[[1]][2])
 }
 SLR_fulltext <- cbind(SLR_fulltext,Field)
-#### search keywords
 
-#### mediation_by study
+#search keywords to extract
+
+## mediation_by study
 
 target_words_medi<-c(" mediate", "Baron & Kenny", "indirect effect", "Sobel", "mediation", "Baron and Kenny")
 medi_1<- matrix(0,nrow=nrow(SLR),ncol=length(target_words_medi))
@@ -194,7 +153,7 @@ SLR
 
 
 
-#### linear model_fulltext
+## linear model_fulltext
 target_words_model<-c("logistic probit","logistic-probit","log linear","log-linear",
                 "multinomial","poisson","negative binomial","cox proportional hazards",
                 "additive hazards","non-linear","non linear","generalized additive","loess","polynomial",
@@ -216,7 +175,7 @@ model_sum<-rowSums(model)
 
 SLR_fulltext <- cbind(SLR_fulltext,model_sum)
 
-####linear model_by_study
+##linear model_by_study
 model_1<- matrix(0,nrow=nrow(SLR),ncol=length(target_words_model))
 
 for (i in 1:nrow(SLR)){
@@ -232,7 +191,7 @@ model_sum_1<-rowSums(model_1)
 SLR <- cbind(SLR,model_sum_1)
 SLR
 
-###sensitivity analysis
+## sensitivity analysis
 target_word2<-"sensitivity analysis"
 paper_fnl2<-list()
 sensitivity<- rep(0,length(paper_fnl_text))
@@ -261,7 +220,7 @@ for (i in 1:nrow(SLR)){
 
 SLR <- cbind(SLR,sensitivity)
 
-###NDE,CDE
+## NDE,CDE
 target_words_DE<-c("natural direct effect","controlled direct effect","natural indirect effect")
 paper_fnl2<-list()
 DE<- matrix(0,nrow=length(paper_fnl_text),ncol=length(target_words_DE))
@@ -298,7 +257,7 @@ DE_sum<-rowSums(DE)
 
 SLR <- cbind(SLR,DE_sum)
 
-###alternative methods
+## alternative methods
 target_words_AM<-c("instrumental variable","front-door","front door")
 paper_fnl2<-list()
 AM<- matrix(0,nrow=length(paper_fnl_text),ncol=length(target_words_AM))
@@ -335,7 +294,7 @@ AlternativeMethods_sum<-rowSums(AM)
 
 SLR <- cbind(SLR,AlternativeMethods_sum)
 
-#### RCT
+## RCT
 target_words_RCT<-c('randomised controlled trial','randomized controlled trial',
                     'controlled design','randomly assigned','controlled study','controlled trial',
                      'randomised','randomized','randomly',
@@ -377,6 +336,7 @@ SLR <- cbind(SLR,RCT_sum)
 SLR_fulltext_1 <- SLR_fulltext
 SLR_1 <- SLR
 
+# save the result
 write.csv(SLR,file="SLR_bystudy_2024.csv")
 
 write.csv(SLR_fulltext,file="SLR_fulltext_2024.csv")
